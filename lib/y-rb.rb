5c7204ca0e26c6d76c76c8ab6a7d8678bf62db51
Hannes Moser <box@hannesmoser.at>
2022-10-10 16:16:09 +0200

    fix: second attempt to fix autoloading
    The previous fix used underscore instead of a dash in the file that
    marks the entrypoint for the library.

---
+++ lib/y-rb.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# load native extension
+begin
+  ruby_version = /(\d+\.\d+)/.match(::RUBY_VERSION)
+  require_relative "#{ruby_version}/yrb"
+rescue LoadError
+  require "yrb"
+end
+
+require_relative "y/array"
+require_relative "y/doc"
+require_relative "y/map"
+require_relative "y/text"
+require_relative "y/xml"
+require_relative "y/transaction"
+require_relative "y/version"
+
+module Y
+end
